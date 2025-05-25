8886fb6d645f10e05940beba2067135db3e74fc2
leejaina <leejaina@konkuk.ac.kr>
2025-05-25 14:40:29 +0900

    feat: NoticeTopAppBar 컴포저블 만듬

--- app/src/main/java/com/ganaljigi/kubf/ui/helper/component/NoticeComponent.kt
+++ app/src/main/java/com/ganaljigi/kubf/ui/helper/component/NoticeComponent.kt
@@ -4,23 +4,100 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+//Helper화면의 NoticeBox
 @Composable
 fun NoticeBox(
-
+    onNavigateClick: () -> Unit
 ) {
     Column(
         modifier = Modifier
     ) {
         Row(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .height(52.dp),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
+            Text(
+                text = "공지사항",
+                fontWeight = FontWeight.SemiBold
+            )
 
+            IconButton(onClick = onNavigateClick) {
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowRight,
+                    contentDescription = "공지사항 페이지로 이동"
+                )
+            }
         }
     }
+}
+
+//Notice화면의 공지사항 TopAppBar
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NoticeTopAppBar(
+    onBackClick: () -> Unit
+) {
+    TopAppBar(
+        title={
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "공지사항",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Color.Black,
+                    //fontFamily = FontFamily(Font(R.font.pretendard_semibold))
+                )
+            }
+        },
+        navigationIcon = {
+            IconButton(onClick = onBackClick) {
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowLeft,
+                    contentDescription = "뒤로가기",
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color.White
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewNoticeTopAppBar() {
+    NoticeTopAppBar (
+        onBackClick = {}
+    )
 }
