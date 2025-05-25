95f92b66b7222ceeb52b9900f450de77e45c1bb9
leejaina <leejaina@konkuk.ac.kr>
2025-05-25 14:26:22 +0900

    feat: NoticeComponent, ShortComponent 파일 만듬

---
+++ app/src/main/java/com/ganaljigi/kubf/ui/helper/component/NoticeComponent.kt
@@ -0,0 +1,26 @@
+package com.ganaljigi.kubf.ui.helper.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun NoticeBox(
+
+) {
+    Column(
+        modifier = Modifier
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/ganaljigi/kubf/ui/helper/component/ShortCutComponent.kt
@@ -0,0 +1,2 @@
+package com.ganaljigi.kubf.ui.helper.component
+
