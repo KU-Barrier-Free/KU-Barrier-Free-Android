29297a2c64ea401bcceb069f904721da3435aeae
leejaina <leejaina@konkuk.ac.kr>
2025-05-26 15:25:13 +0900

    feat: 바로가기 제목 박스 ShortCutTitle 만듬

--- app/src/main/java/com/ganaljigi/kubf/ui/helper/component/shortcut/ShortCutComponent.kt
+++ app/src/main/java/com/ganaljigi/kubf/ui/helper/component/shortcut/ShortCutComponent.kt
@@ -1,6 +1,49 @@
 package com.ganaljigi.kubf.ui.helper.component.shortcut
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 //바로가기 제목 박스
+@Composable
+fun ShortCutTitle() {
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .height(52.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "바로가기",
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 18.sp
+        )
+    }
+}
 
 //바로가기 작은 박스
 
