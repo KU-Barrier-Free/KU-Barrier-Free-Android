e07a6ba7b6f360ffdcdea003a36d088643625e3b
leejaina <leejaina@konkuk.ac.kr>
2025-05-29 11:09:22 +0900

    feat: HelperScreen 만듬

---
+++ app/src/main/java/com/ganaljigi/kubf/ui/helper/screen/HelperScreen.kt
@@ -0,0 +1,27 @@
+package com.ganaljigi.kubf.ui.helper.screen
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.ganaljigi.kubf.ui.helper.component.NoticeWebView
+import com.ganaljigi.kubf.ui.helper.component.notice.NoticeTopAppBar
+
+@Composable
+fun HelperScreen(
+    onBackClick: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            NoticeTopAppBar(onBackClick = onBackClick)
+        }
+    ) { innerPadding ->
+        NoticeWebView(
+            url = "",
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        )
+    }
+}
