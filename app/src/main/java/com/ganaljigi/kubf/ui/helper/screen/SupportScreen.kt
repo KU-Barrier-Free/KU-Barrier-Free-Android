6b568b0fbcb7799c57b92ae1f52de84cca5f6791
leejaina <leejaina@konkuk.ac.kr>
2025-05-29 11:08:11 +0900

    feat: SupportScreen 만듬

---
+++ app/src/main/java/com/ganaljigi/kubf/ui/helper/screen/SupportScreen.kt
@@ -0,0 +1,27 @@
+package com.ganaljigi.kubf.ui.helper.screen
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.ganaljigi.kubf.ui.helper.component.NoticeWebView
+import com.ganaljigi.kubf.ui.helper.component.notice.NoticeTopAppBar
+
+@Composable
+fun SupportScreen(
+    onBackClick: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            NoticeTopAppBar(onBackClick = onBackClick)
+        }
+    ) { innerPadding ->
+        NoticeWebView(
+            url = "https://www.konkuk.ac.kr/csd/15234/subview.do",
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        )
+    }
+}
