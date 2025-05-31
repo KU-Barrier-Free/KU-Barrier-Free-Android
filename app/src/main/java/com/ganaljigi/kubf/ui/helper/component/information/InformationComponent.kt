1dc28423810529a401c88c4970da194cff01909e
leejaina <leejaina@konkuk.ac.kr>
2025-05-31 16:27:04 +0900

    feat: 정보 박스에 들어갈 정보 아이템 InfoItemBox 맨듬

--- app/src/main/java/com/ganaljigi/kubf/ui/helper/component/information/InformationComponent.kt
+++ app/src/main/java/com/ganaljigi/kubf/ui/helper/component/information/InformationComponent.kt
@@ -1,19 +1,26 @@
 package com.ganaljigi.kubf.ui.helper.component.information
 
+import android.graphics.drawable.Icon
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SegmentedButtonDefaults.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -40,6 +47,47 @@ fun InformationTitle() {
 }
 
 //정보 하나 박스
+@Composable
+fun InfoItemBox(
+    icon: ImageVector,
+    label: String
+) {
+    Box(
+        modifier = Modifier
+            .size(width = 304.dp, height = 92.dp)
+            .padding(horizontal = 12.dp)
+    ) {
+        Row {
+            //작은 박스1: 아이콘, 정보 종류 제목
+            Row(
+                modifier = Modifier
+                    .size(width = 80.dp, height = 20.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector=icon,
+                    contentDescription = null,
+                    modifier = Modifier.size(20.dp)
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = label,
+                    style = KUBFAndroidTheme.typography.semiBold14.copy(
+                        fontSize = 14.sp
+                    )
+                )
+            }
+            Spacer(modifier = Modifier.width(12.dp))
+
+            //작은 박스2: 안에 설명들?!
+            Box(
+                modifier = Modifier.fillMaxHeight()
+            ) {
+
+            }
+        }
+    }
+}
 
 //지도 박스
 @Composable
