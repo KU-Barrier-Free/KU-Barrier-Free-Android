13ddab09d6b6a429089f9430e1f7a5208200e858
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-07-01 15:48:51 +0900

    feat: 홈 화면에 지도 컴포넌트 삽입 #11

--- app/src/main/java/com/ganaljigi/kubf/ui/home/component/BarrierFreeInfoItem.kt
+++ app/src/main/java/com/ganaljigi/kubf/ui/home/component/BarrierFreeInfoItem.kt
@@ -36,8 +36,8 @@ import com.ganaljigi.kubf.ui.util.noRippleClickable
 @Composable
 fun BarrierFreeInfoItem(
     modifier: Modifier = Modifier,
-    onClick: () -> Unit = { },
-    visible: Boolean = false
+    visible: Boolean = false,
+    onClick: () -> Unit = { }
 ) {
     AnimatedVisibility(
         visible = visible,
