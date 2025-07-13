451cf1e812910562c65ae178667c4e5603358dfb
leejaina <leejaina@konkuk.ac.kr>
2025-07-14 00:28:33 +0900

    feat: 지원업무 페이지 TabRow 추가

--- app/src/main/java/com/ganaljigi/kubf/ui/helper/screen/SupportScreen.kt
+++ app/src/main/java/com/ganaljigi/kubf/ui/helper/screen/SupportScreen.kt
@@ -1,30 +1,70 @@
 package com.ganaljigi.kubf.ui.helper.screen
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.ganaljigi.kubf.ui.helper.component.WebViewTopAppBar
 import com.ganaljigi.kubf.ui.helper.component.NoticeWebView
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 @Composable
 fun SupportScreen(
     onBackClick: () -> Unit
 ) {
-    Scaffold(
-        topBar = {
-            WebViewTopAppBar(
-                textTitle = "지원 업무",
-                onBackClick = onBackClick
+    var tabIndex by remember { mutableStateOf(0) }
+
+    Column (modifier = Modifier.fillMaxSize()) {
+        WebViewTopAppBar(
+            textTitle = "지원 업무",
+            onBackClick = onBackClick
+        )
+
+        TabRow(selectedTabIndex = tabIndex) {
+            Tab(
+                selected = tabIndex == 0,
+                onClick = { tabIndex = 0 },
+                text = { Text("교수/학습") }
             )
+            Tab(
+                selected = tabIndex == 1,
+                onClick = { tabIndex = 1 },
+                text = { Text("기자재") }
+            )
+            Tab(
+                selected = tabIndex == 2,
+                onClick = { tabIndex = 2 },
+                text = { Text("장학 제도") }
+            )
+            Tab(
+                selected = tabIndex == 3,
+                onClick = { tabIndex = 3 },
+                text = { Text("시설 현황") }
+            )
+
         }
-    ) { innerPadding ->
+
+        val url = when (tabIndex) {
+            0 -> ""
+            1 -> ""
+            2 -> ""
+            3 -> ""
+            else -> ""
+        }
+
         NoticeWebView(
-            url = "https://www.konkuk.ac.kr/csd/15234/subview.do",
+            url = url,
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding)
+                .weight(1f)
         )
     }
 }
