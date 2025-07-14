91ae405a882ba6c06963f53049c7c81dddecb3f2
leejaina <leejaina@konkuk.ac.kr>
2025-07-14 21:04:37 +0900

    refactor: webView 컴포저블에 key 추가

--- app/src/main/java/com/ganaljigi/kubf/ui/helper/component/HelperWebView.kt
+++ app/src/main/java/com/ganaljigi/kubf/ui/helper/component/HelperWebView.kt
@@ -3,19 +3,22 @@ package com.ganaljigi.kubf.ui.helper.component
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.key
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 
 @Composable
 fun NoticeWebView(url: String, modifier: Modifier = Modifier) {
-    AndroidView(
-        modifier = modifier,
-        factory = { context ->
-            WebView(context).apply {
-                settings.javaScriptEnabled = true
-                webViewClient = WebViewClient()
-                loadUrl(url)
+    key(url) {
+        AndroidView(
+            modifier = modifier,
+            factory = { context ->
+                WebView(context).apply {
+                    settings.javaScriptEnabled = true
+                    webViewClient = WebViewClient()
+                    loadUrl(url)
+                }
             }
-        }
-    )
+        )
+    }
 }
