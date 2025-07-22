85fa8c72f867d74d99bdc78d86a9499347a421c3
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-07-22 15:44:24 +0900

    feat: 바뀐 홈 화면 UI 적용 #17

--- app/src/main/java/com/ganaljigi/kubf/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/ganaljigi/kubf/ui/home/screen/HomeScreen.kt
@@ -1,8 +1,6 @@
 package com.ganaljigi.kubf.ui.home.screen
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
@@ -13,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -22,8 +19,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.TextFieldValue
@@ -35,11 +31,12 @@ import com.ganaljigi.kubf.ui.home.component.BarrierFreeInfoChip
 import com.ganaljigi.kubf.ui.home.component.BarrierFreeInfoItem
 import com.ganaljigi.kubf.ui.home.component.FindWayButton
 import com.ganaljigi.kubf.ui.home.component.HomeSearchBar
+import com.ganaljigi.kubf.ui.home.component.HomeSearchContent
+import com.ganaljigi.kubf.ui.home.component.HomeSearchTopBar
 import com.ganaljigi.kubf.ui.home.component.HomeToggle
 import com.ganaljigi.kubf.ui.home.component.MapComponent
 import com.ganaljigi.kubf.ui.home.component.NoticeButton
 import com.ganaljigi.kubf.ui.home.viewmodel.ToggleUiState
-import com.ganaljigi.kubf.ui.theme.KUBFAndroidTheme
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.rememberCameraPositionState
@@ -68,39 +65,38 @@ fun HomeScreen(
     var isBarrierFreeShown by remember {
         mutableStateOf(false)
     }
-    val focusRequester = remember { FocusRequester() }
-    val focusManager = LocalFocusManager.current
-
     val konkukUniversity = LatLng(37.5407, 127.0785)
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(konkukUniversity, 16f)
     }
+    var isSearchMode by remember { mutableStateOf(false) }
+    val focusRequester = remember { FocusRequester() }
+    val focusManager = LocalFocusManager.current
 
     Scaffold(
         modifier = Modifier
             .fillMaxSize()
             .padding(padding),
         topBar = {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color.White)
-                    .padding(horizontal = 20.dp, vertical = 15.dp)
-            ) {
-                Text(
-                    text = "KU-Barrier Free",
-                    style = KUBFAndroidTheme.typography.medium20
+            if (isSearchMode) {
+                HomeSearchTopBar(
+                    onClick = {
+                        isSearchMode = false
+                        focusManager.clearFocus()
+                    }
                 )
             }
         }
     ) { innerPadding ->
 
-        MapComponent(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding),
-            cameraPosition = cameraPositionState
-        )
+        if (!isSearchMode) {
+            MapComponent(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+                cameraPosition = cameraPositionState
+            )
+        }
 
         Column(
             modifier = Modifier
@@ -113,13 +109,15 @@ fun HomeScreen(
                 Row(
                     modifier = Modifier
                         .height(IntrinsicSize.Min)
-                        .padding(vertical = 8.dp),
+                        .padding(top = 12.dp, bottom = 8.dp),
                     horizontalArrangement = Arrangement.spacedBy(8.dp),
                 ) {
                     HomeSearchBar(
                         modifier = Modifier
                             .weight(1f)
-                            .focusRequester(focusRequester),
+                            .onFocusChanged { focusState ->
+                                isSearchMode = focusState.isFocused
+                            },
                         onValueChange = { searchValue = it },
                         onValueCleared = { searchValue = TextFieldValue("") },
                         onChipClick = { searchKeyword ->
@@ -127,51 +125,57 @@ fun HomeScreen(
                                 text = searchKeyword.label,
                                 selection = TextRange(searchKeyword.label.length)
                             )
-                            focusManager.clearFocus()
                         },
                         onSearchKeyboardClick = {
                             // TODO:  검색 기능
-                            focusManager.clearFocus()
                         },
                         value = searchValue,
                         searchKeywordEntry = SearchKeyword.entries
                     )
-                    FindWayButton(
-                        modifier = Modifier.fillMaxHeight()
-                    ) { // TODO: 길찾기  }
+                    if (!isSearchMode) {
+                        FindWayButton(
+                            modifier = Modifier.fillMaxHeight()
+                        ) { /* TODO: 길찾기  */ }
                     }
                 }
-
-                HomeToggle(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    toggleUiStates = toggleUiState,
-                    onToggleClick = { toggle ->
-                        toggleUiState = toggleUiState.toMutableList()
-                            .map { if (it.toggle == toggle) it.copy(isSelected = !it.isSelected) else it }
-                            .toList()
-                    }
-                )
+                if (!isSearchMode) {
+                    HomeToggle(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        toggleUiStates = toggleUiState,
+                        onToggleClick = { toggle ->
+                            toggleUiState = toggleUiState.toMutableList()
+                                .map { if (it.toggle == toggle) it.copy(isSelected = !it.isSelected) else it }
+                                .toList()
+                        }
+                    )
+                }
+                if (isSearchMode) {
+                    HomeSearchContent()
+                }
             }
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 20.dp),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.Bottom
-            ) {
-                BarrierFreeInfoItem(
-                    visible = isBarrierFreeShown,
+
+            if (!isSearchMode) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 20.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.Bottom
                 ) {
-                    isBarrierFreeShown = !isBarrierFreeShown
-                }
-                if (!isBarrierFreeShown) {
-                    BarrierFreeInfoChip {
+                    BarrierFreeInfoItem(
+                        visible = isBarrierFreeShown,
+                    ) {
                         isBarrierFreeShown = !isBarrierFreeShown
                     }
+                    if (!isBarrierFreeShown) {
+                        BarrierFreeInfoChip {
+                            isBarrierFreeShown = !isBarrierFreeShown
+                        }
 
-                    NoticeButton {
-                        navigateToHelper()
+                        NoticeButton {
+                            navigateToHelper()
+                        }
                     }
                 }
             }
