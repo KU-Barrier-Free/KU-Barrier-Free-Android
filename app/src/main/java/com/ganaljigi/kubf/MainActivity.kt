887077d57fe60a2568bcf1282e93bdf22648f6e8
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-07-23 21:12:56 +0900

    feat: Hilt App 적용 #17

--- app/src/main/java/com/ganaljigi/kubf/MainActivity.kt
+++ app/src/main/java/com/ganaljigi/kubf/MainActivity.kt
@@ -9,26 +9,25 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.ganaljigi.kubf.navigation.MainNavHost
-import com.ganaljigi.kubf.ui.helper.screen.HelperScreen
 import com.ganaljigi.kubf.ui.theme.KUBFAndroidTheme
+import dagger.hilt.android.AndroidEntryPoint
 
-//import com.kakao.sdk.common.util.Utility
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             KUBFAndroidTheme {
-//                val navController = rememberNavController()
-//
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    MainNavHost(
-//                        padding = innerPadding,
-//                        navController = navController,
-//                    )
-//                }
-                HelperScreen {  }
+                val navController = rememberNavController()
+
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainNavHost(
+                        padding = innerPadding,
+                        navController = navController,
+                    )
+                }
             }
         }
     }
