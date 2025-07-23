0afacdee8524f7a647f6bf1bf0d3af35b01d6f76
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-07-23 15:14:13 +0900

    feat: HomeScreen 에 isSearchMode 제거 #17

--- app/src/main/java/com/ganaljigi/kubf/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/ganaljigi/kubf/ui/home/screen/HomeScreen.kt
@@ -10,12 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -25,18 +20,13 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextRange
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ganaljigi.kubf.ui.common.model.MapToggle
 import com.ganaljigi.kubf.ui.common.model.SearchKeyword
-import com.ganaljigi.kubf.ui.common.model.SearchResult
 import com.ganaljigi.kubf.ui.home.component.BarrierFreeInfoChip
 import com.ganaljigi.kubf.ui.home.component.BarrierFreeInfoItem
 import com.ganaljigi.kubf.ui.home.component.FindWayButton
@@ -45,10 +35,8 @@ import com.ganaljigi.kubf.ui.home.component.MapComponent
 import com.ganaljigi.kubf.ui.home.component.NoticeButton
 import com.ganaljigi.kubf.ui.home.component.bottomsheet.HomeSearchBottomSheet
 import com.ganaljigi.kubf.ui.home.component.search.HomeSearchBar
-import com.ganaljigi.kubf.ui.home.component.search.HomeSearchContent
-import com.ganaljigi.kubf.ui.home.component.search.HomeSearchTopBar
 import com.ganaljigi.kubf.ui.home.viewmodel.ToggleUiState
-import com.ganaljigi.kubf.ui.theme.MainGreen
+import com.ganaljigi.kubf.ui.theme.KUBFAndroidTheme
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.rememberCameraPositionState
@@ -82,52 +70,46 @@ fun HomeScreen(
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(konkukUniversity, 16f)
     }
-    var isSearchMode by remember { mutableStateOf(false) }
-    val focusManager = LocalFocusManager.current
     var showSearchBottomSheet by remember { mutableStateOf(false) }
     val sheetState = rememberModalBottomSheetState()
     val scope = rememberCoroutineScope()
+    var isFindMode by remember { mutableStateOf(false) }
+    val focusManager = LocalFocusManager.current
 
-    Scaffold(
+
+    if (showSearchBottomSheet) {
+        HomeSearchBottomSheet(
+            sheetState = sheetState,
+            onDismissRequest = { showSearchBottomSheet = false },
+            searchResults = emptyList(),
+        )
+    }
+
+    MapComponent(
         modifier = Modifier
             .fillMaxSize()
             .padding(padding),
-        topBar = {
-            if (isSearchMode) {
-                HomeSearchTopBar(
-                    onClick = {
-                        isSearchMode = false
-                        focusManager.clearFocus()
-                    }
-                )
-            }
-        },
-    ) { innerPadding ->
+        cameraPosition = cameraPositionState
+    )
 
-        if (showSearchBottomSheet) {
-            HomeSearchBottomSheet(
-                sheetState = sheetState,
-                onDismissRequest = { showSearchBottomSheet = false },
-                searchResults = emptyList(),
-            )
-        }
-
-        if (!isSearchMode) {
-            MapComponent(
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(padding)
+            .padding(horizontal = 16.dp),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        if (isFindMode) {
+            Row(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .padding(innerPadding),
-                cameraPosition = cameraPositionState
-            )
-        }
-
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-                .padding(horizontal = 16.dp),
-            verticalArrangement = Arrangement.SpaceBetween
-        ) {
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Column(
+//                        modifier = Modifier.
+                ) { }
+            }
+        } else {
             Column {
                 Row(
                     modifier = Modifier
@@ -137,10 +119,7 @@ fun HomeScreen(
                 ) {
                     HomeSearchBar(
                         modifier = Modifier
-                            .weight(1f)
-                            .onFocusChanged { focusState ->
-                                isSearchMode = focusState.isFocused
-                            },
+                            .weight(1f),
                         onValueChange = { searchValue = it },
                         onValueCleared = { searchValue = TextFieldValue("") },
                         onChipClick = { searchKeyword ->
@@ -155,50 +134,45 @@ fun HomeScreen(
                         value = searchValue,
                         searchKeywordEntry = SearchKeyword.entries
                     )
-                    if (!isSearchMode) {
-                        FindWayButton(
-                            modifier = Modifier.fillMaxHeight()
-                        ) { /* TODO: 길찾기  */ }
-                    }
+                    FindWayButton(
+                        modifier = Modifier.fillMaxHeight()
+                    ) { /* TODO: 길찾기  */ }
                 }
-                if (!isSearchMode) {
-                    HomeToggle(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        toggleUiStates = toggleUiState,
-                        onToggleClick = { toggle ->
-                            toggleUiState = toggleUiState.toMutableList()
-                                .map { if (it.toggle == toggle) it.copy(isSelected = !it.isSelected) else it }
-                                .toList()
-                        }
-                    )
-                }
-                if (isSearchMode) {
-                    HomeSearchContent()
-                }
-            }
-
-            if (!isSearchMode) {
-                Row(
+                HomeToggle(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 20.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.Bottom
+                        .fillMaxWidth(),
+                    toggleUiStates = toggleUiState,
+                    onToggleClick = { toggle ->
+                        toggleUiState = toggleUiState.toMutableList()
+                            .map { if (it.toggle == toggle) it.copy(isSelected = !it.isSelected) else it }
+                            .toList()
+                        focusManager.clearFocus()
+                    }
+                )
+            }
+        }
+
+        // 기본 모드일 경우에만 보임
+        if (!isFindMode) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 20.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.Bottom
+            ) {
+                BarrierFreeInfoItem(
+                    visible = isBarrierFreeShown,
                 ) {
-                    BarrierFreeInfoItem(
-                        visible = isBarrierFreeShown,
-                    ) {
+                    isBarrierFreeShown = !isBarrierFreeShown
+                }
+                if (!isBarrierFreeShown) {
+                    BarrierFreeInfoChip {
                         isBarrierFreeShown = !isBarrierFreeShown
                     }
-                    if (!isBarrierFreeShown) {
-                        BarrierFreeInfoChip {
-                            isBarrierFreeShown = !isBarrierFreeShown
-                        }
 
-                        NoticeButton {
-                            navigateToHelper()
-                        }
+                    NoticeButton {
+                        navigateToHelper()
                     }
                 }
             }
@@ -206,10 +180,12 @@ fun HomeScreen(
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun HomeScreenPreview() {
-    HomeScreen(
-        padding = PaddingValues(0.dp)
-    )
+    KUBFAndroidTheme {
+        HomeScreen(
+            padding = PaddingValues(0.dp)
+        )
+    }
 }
