8ca1118134655c3917c9c5f972a3b3ec414ed7bf
leejaina <leejaina@konkuk.ac.kr>
2025-07-28 17:15:26 +0900

    refactor: TopAppBar 아이콘 추가 #16

--- app/src/main/java/com/ganaljigi/kubf/ui/roominfo/TopAppBar.kt
+++ app/src/main/java/com/ganaljigi/kubf/ui/roominfo/TopAppBar.kt
@@ -1,9 +1,12 @@
 package com.ganaljigi.kubf.ui.roominfo
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material.icons.Icons
@@ -12,6 +15,7 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -19,8 +23,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.ganalijigi.kubf.R
 import com.ganaljigi.kubf.ui.theme.KUBFAndroidTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -29,40 +36,34 @@ fun RoomInfoTopAppBar(
     buildingName: String,
     onBackClick: () -> Unit
 ) {
-    CenterAlignedTopAppBar(
-        title = {
-            Row(
-                modifier = Modifier.fillMaxHeight(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = "${buildingName} 공간",
-                    style = KUBFAndroidTheme.typography.medium14.copy(
-                        fontSize = 16.sp
-                    )
-                )
-            }
-        },
-        navigationIcon = {
+    Surface(
+        color = Color.White
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(48.dp)
+        ) {
             IconButton(
                 onClick = onBackClick,
-                modifier = Modifier.fillMaxHeight()
+                modifier = Modifier
+                    .align(Alignment.CenterStart)
             ) {
                 Icon(
-                    imageVector = Icons.Default.KeyboardArrowLeft,
+                    painter = painterResource(id = R.drawable.ic_backarrow),
                     contentDescription = "뒤로가기",
-                    modifier = Modifier
-                        .fillMaxHeight()
+                    modifier = Modifier.size(24.dp)
                 )
             }
-        },
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = Color.White
-        ),
-        modifier = Modifier
-            .fillMaxWidth()
-            .windowInsetsPadding(WindowInsets.statusBars)
-    )
+            Text(
+                text = "$buildingName 공간",
+                style = KUBFAndroidTheme.typography.medium14.copy(
+                    fontSize = 16.sp
+                ),
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
