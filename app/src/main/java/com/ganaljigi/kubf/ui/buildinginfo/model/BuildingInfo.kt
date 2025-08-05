f2d90d8fd620008f870d6f8282f33142ee5dada9
sskkyy0 <zxzxzxzxzxzxzz@naver.com>
2025-08-05 17:01:00 +0900

    refactor: data class 들 전부 model 패키지로 이동

---
+++ app/src/main/java/com/ganaljigi/kubf/ui/buildinginfo/model/BuildingInfo.kt
@@ -0,0 +1,9 @@
+package com.ganaljigi.kubf.ui.buildinginfo.model
+
+data class BuildingInfo(
+    val name: String,
+    val number: Int,
+    val department: String,
+    val imageUrl: String,
+    val notes: Notes
+)
