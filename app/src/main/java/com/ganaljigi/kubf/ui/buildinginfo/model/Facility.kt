292f59376a01e34b18b1ddf4c3adc6bfab2fca4f
sskkyy0 <zxzxzxzxzxzxzz@naver.com>
2025-09-12 11:10:06 +0900

    refactor: 주요시설 추가

--- app/src/main/java/com/ganaljigi/kubf/ui/buildinginfo/model/Facility.kt
+++ app/src/main/java/com/ganaljigi/kubf/ui/buildinginfo/model/Facility.kt
@@ -6,11 +6,16 @@ import com.ganalijigi.kubf.R
 enum class Facility(val label: String, @DrawableRes val iconResId:Int){
     CAFE("카페", R.drawable.ic_feature_cafe),
     CONV("편의점", R.drawable.ic_feature_conv),
-    PRINT("복사기", R.drawable.ic_feature_print),
+    PRINT("복사실", R.drawable.ic_feature_print),
     REST("휴게실", R.drawable.ic_feature_rest),
     KCUBE("K-CUBE", R.drawable.ic_feature_kcube),
     KHUB("K-Hub",R.drawable.ic_feature_kcube),
     SERVICE("IT-서비스센터", R.drawable.ic_feature_itser),
     PARK("주차장", R.drawable.ic_feature_park),
     BANK("은행",R.drawable.ic_feature_itser),
+    ELEV("엘리베이터",R.drawable.ic_building),
+    POST("우체국",R.drawable.ic_toggle_special),
+    TOILET("장애인화장실",R.drawable.ic_toggle_special),
+    CULF("문화시설",R.drawable.ic_toggle_special),
+    STORE("복지매장",R.drawable.ic_toggle_special)
 }
